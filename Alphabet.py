5505e77f37a7f54bc4308460d6911e69e984c2a3
ggpradeep <80970765+ggpradeep@users.noreply.github.com>
2026-05-19 19:32:39 -0700

    Add files via upload

---
+++ Alphabet.py
@@ -0,0 +1,6 @@
+check = str(input("Enter a LOWERCASE character"))
+alphabet = "a","b","c","d,",'e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+if check == alphabet:
+    print("This character is a part of the alphabet.")
+else:
+    print("This character is not part of the alphabet.")
